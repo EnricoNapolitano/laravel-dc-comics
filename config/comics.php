c283c84a30660192589bc0aa560bf61dc78b6841
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-28 18:14:24 +0100

    changing link to images in comics.php

---
+++ config/comics.php
@@ -0,0 +1,87 @@
+<?php
+return [
+        [
+          'thumb' =>
+            'https://static.dc.com/dc/files/default_images/AC_1000_300-001_HD_5b7ef32cb88c15.49925767.jpg?w=600',
+          'price' => '19.99',
+          'series' => 'Action Comics',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+             'https://static.dc.com/dc/files/default_images/AMVAMP1976%2520%2528COVER%2529_6179b409d52e93.54241046.jpg?w=600',
+          'price' => '3.99',
+          'series' => 'American Vampire 1976',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+             'https://static.dc.com/dc/files/default_images/AQMLEGAQM_176-001_HD_5b7ef95f2f4b75.83113919.jpg?w=600',
+          'price' => '16.99',
+          'series' => 'Aquaman',
+          'type' => 'graphic novel',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/batgirl_v3_3_5b15cc3ef22748.83097205.jpg?w=600',
+          'price' => '2.99',
+          'series' => 'Batgirl',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/BM_111%2520%2528COVER%2529_61004eba155cb2.29629294.jpg?w=600',
+          'price' => '3.99',
+          'series' => 'Batman',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/bmbeyond_hushbeyond_5b071c80d72ce3.43932324.jpg?w=600',
+          'price' => '2.99',
+          'series' => 'Batman Beyond',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/Batman-Superman-v1-cross-world-cvr_53bdcbfd5c6256.94452792.jpg?w=600',
+          'price' => '3.99',
+          'series' => 'Batman/Superman',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/BMSMANN2021_01%2520%2528BM%2520COVER%2529_611db4bc8d7de4.09759430.jpg?w=600',
+          'price' => '4.99',
+          'series' => 'Batman/Superman Annual',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/JWS_DJ_6019d63099a962.73351174.jpg?w=600',
+          'price' => '5.99',
+          'series' => 'Batman: The Joker War Zone',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/BTJ__5fa0aec041ddb7.35141764.jpg?w=600',
+          'price' => '6.99',
+          'series' => 'Batman: Three Jokers',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/BMWK_HQ%2520%2528COVER%2529_60ca8a6db3da64.48239084.jpg?w=600',
+          'price' => '4.99',
+          'series' => 'Batman: White Knight Presents: Harley Quinn',
+          'type' => 'comic book',
+        ],
+        [
+          'thumb' =>
+          'https://static.dc.com/dc/files/default_images/catwoman_v4_1_5b1703e5e7ca45.32433448.jpg?w=600',
+          'price' => '16.99',
+          'series' => 'Catwoman',
+          'type' => 'graphic novel',
+        ],
+    ];
