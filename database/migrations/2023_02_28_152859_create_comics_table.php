77703d399a2b7f5e4c7210d6533674652664cf45
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-28 18:14:53 +0100

    comics table

--- database/migrations/2023_02_28_152859_create_comics_table.php
+++ database/migrations/2023_02_28_152859_create_comics_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('thumb');
+            $table->float('price');
+            $table->string('series');
+            $table->string('type');
             $table->timestamps();
         });
     }
