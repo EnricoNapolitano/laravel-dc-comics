be2d8434c43239e1e549ee0728cb065bc8580dc0
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-01 18:21:44 +0100

    route to store data on database

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,8 @@ Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 //Route to create
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 
+//Route to storage
+Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
+
 //Route for specific comic
 Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
