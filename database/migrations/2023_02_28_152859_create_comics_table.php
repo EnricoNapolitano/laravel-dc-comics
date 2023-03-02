05837a540389c0c717c279539b7b83f1c0a5d7d7
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-02 21:53:50 +0100

    description not anymore nullable

--- database/migrations/2023_02_28_152859_create_comics_table.php
+++ database/migrations/2023_02_28_152859_create_comics_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('description')->nullable();
+            $table->text('description');
             $table->string('thumb')->nullable();
             $table->float('price', 4, 2);
             $table->string('series');
