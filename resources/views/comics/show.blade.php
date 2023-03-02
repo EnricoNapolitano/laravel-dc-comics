125d8a412bcf13e06f84bebab1a2b7de95ab9584
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-02 19:02:55 +0100

    comics.show: buttons to edit and delete

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,13 +1,21 @@
 @extends('layouts.app')
 @section('content')
-<div class="container text-white flex mh-5">
+<div class="container text-white flex as ph-5">
     <figure>
         <img src="{{$comic->thumb}}" alt="{{$comic->title}}" class="img-fluid">
     </figure>
     <div class="pl-3">
         <h1>{{$comic->title}}</h1>
         <h3>${{$comic->price}}</h3>
-        <div>{{$comic->description}}</div>
+        <p class="mb-3">{{$comic->description}}</p>
+        <div class="flex">
+            <a href="{{ route('comics.edit', $comic) }}" class="bg-primary text-white pw-3 ph-1"><b>EDIT</b></a>
+            <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+            @csrf
+            @method('DELETE')
+                <button class="btn-size bolder bg-custom2 text-white p-btn ml-2">DELETE</button>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
